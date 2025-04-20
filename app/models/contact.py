3bc5e1f4660746fe23c84c275de9ab4e3212d06d
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-04-20 19:31:32 +0000

    Added comments

--- app/models/contact.py
+++ app/models/contact.py
@@ -4,11 +4,11 @@ from app.utils.db import db
 class Contact(db.Model):
     __tablename__ = 'contacts'
 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False)
-    email = db.Column(db.String(100), nullable = False)
-    message = db.Column(db.Text, nullable = False)
-    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
+    id = db.Column(db.Integer, primary_key = True) # Unique ID for each message (PK)
+    name = db.Column(db.String(100), nullable = False) # Full name of the person contacting
+    email = db.Column(db.String(100), nullable = False) # Email address of the sender
+    message = db.Column(db.Text, nullable = False) # The content of the message
+    timestamp = db.Column(db.DateTime, default = datetime.utcnow) # Timestamp of when message was sent
 
     def __repr__(self):
         return f'<Contact {self.name}>'

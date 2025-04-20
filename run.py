755e10d72bf054af91005994f067b7e1cb1c45c1
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-04-20 19:40:10 +0000

    Creating the database portfolio.db from the db

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-# run.py
-
 from app import create_app  # Import the create_app function from the app package
-
+from app.utils.db import db
 app = create_app()  # Create the app instance
 
-if __name__ == "__main__":
-    app.run(debug=True)  # Run the app in debug mode
+with app.app_context():
+    db.create_all()
+    print("✅ Tables created!")

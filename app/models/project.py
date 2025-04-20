83fedf3d858c45e32588eb618b10cdc2cf57b41a
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-04-20 19:21:28 +0000

    Created the project model

---
+++ app/models/project.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from app.utils.db import db
+
+class Project(db.model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key = True) # Unique ID for each project (PK)
+    title = db.Column(db.String(150), nullable = False) # Name or title of the project
+    description = db.Column(db.Text, nullable = False) # Short description of the project
+    link = db.Column(db.String(255)) # Optional project link
+    created_at = db.Column(db.DateTime, default = datetime.utcnow)

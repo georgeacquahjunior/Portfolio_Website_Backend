7b0307a31a64a4f10b835866135c6d911fcb3179
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-04-20 23:44:45 +0000

    update on routes and run

--- app/routes/contact_routes.py
+++ app/routes/contact_routes.py
@@ -1,9 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.models.contact import Contact
+from app.utils.db import db
 
 # Define a blueprint for the contact routes
 contact_bp = Blueprint('contact', __name__)
 
 # Define the route for the contact page
-@contact_bp.route('/contact')
-def contact():
-    return "Contact Page"
+@contact_bp.route('/', methods = ['POST'])
+def submit_contact():
+    data = request.get_json()
+
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+
+    if not name or not email or not message:
+        return jsonify({'error': 'All fields are required'}), 400
+    
+    new_contact = Contact(name = name, email = email, message = message)
+    db.session.add(new_contact)
+    db.session.commit()
+
+    return jsonify({'message': 'Contact submitted successfully!'}), 201
--- run.py
+++ run.py
@@ -2,6 +2,5 @@ from app import create_app  # Import the create_app function from the app packag
 from app.utils.db import db
 app = create_app()  # Create the app instance
 
-with app.app_context():
-    db.create_all()
-    print("✅ Tables created!")
+if __name__ == '__main__':
+    app.run(debug=True)
